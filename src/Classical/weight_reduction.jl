271ad4dfe3a603bb6523f1ef069d746eef6e4002
Ben Ide <ben@bencide.com>
2024-02-06 17:11:13 -0500

    fixed a couple of things in `weight_reduction`, added tutorial stub

--- src/Classical/weight_reduction.jl
+++ src/Classical/weight_reduction.jl
@@ -5,7 +5,10 @@
 # LICENSE file in the root directory of this source tree.
 
 function _reduce_rows(H::Union{CTMatrixTypes, MatElem{<: ResElem}}, rows::AbstractVector{Int},
-    compressed::Bool = false, permute::Bool = false)
+    compressed::Bool = false, permute::Bool = false, target::Int = 3)
+
+    # TODO: for compressed, should use the target weight as well so it can be even more compressed if possible.
+    compressed && target > 3 && @warn("Compressed weight-reduction for weights greater than 3 not yet implemented.")
 
     isempty(rows) && return H
     nr, nc = size(H)
@@ -59,38 +62,36 @@ end
 weight_reduction(H::Union{CTMatrixTypes, MatElem{<: ResElem}}; rows::Bool = true,
     row_indices::AbstractVector{Int} = Int[], permute_rows::Bool = true, row_target::Int = 3,
     columns::Bool = true, column_indices::AbstractVector{Int} = Int[], permute_columns::Bool = true,
-    column_target::Int = 3, compressed::Bool = false, seed = nothing)
+    column_target::Int = 3, compressed::Bool = false, seed::Union{Nothing, Int} = nothing)
 
-Return the weight-reduced code of `H` with the given arguments.
+Return the weight-reduced parity-check matrix of `H` with the given arguments.
 """
 function weight_reduction(H::Union{CTMatrixTypes, MatElem{<: ResElem}}; rows::Bool = true,
     row_indices::AbstractVector{Int} = Int[], permute_rows::Bool = true, row_target::Int = 3,
     columns::Bool = true, column_indices::AbstractVector{Int} = Int[], permute_columns::Bool = true,
-    column_target::Int = 3, compressed::Bool = false, seed = nothing)
+    column_target::Int = 3, compressed::Bool = false, seed::Union{Nothing, Int} = nothing)
 
     # method ∈ (:compressed, :identity) || throw(ArgumentError("Unknown reduction method"))
+    all(1 <= r <= nrows(H) for r in row_indices) || throw(ArgumentError("Invalid row indices"))
+    all(1 <= r <= ncols(H) for r in column_indices) || throw(ArgumentError("Invalid col indices"))
     Random.seed!(seed)
 
     if rows
-        if !isempty(row_indices)
-            all(1 <= r <= nrows(H) for r in row_indices) || throw(ArgumentError("Invalid row indices"))
-        else
+        if isempty(row_indices)
             _, row_wts = _degree_distribution(H)
             row_indices = findall(x -> x > row_target, row_wts)
         end
-        H_reduced = _reduce_rows(H, row_indices, compressed, permute_rows)
+        H_reduced = _reduce_rows(H, row_indices, compressed, permute_rows, row_target)
     else
         H_reduced = H
     end
 
     if columns
-        if !isempty(column_indices)
-            all(1 <= r <= nrows(H) for r in row_indices) || throw(ArgumentError("Invalid row indices"))
-        else
+        if isempty(column_indices)
             col_wts, _ = _degree_distribution(H_reduced)
             column_indices = findall(x -> x > column_target, col_wts)
         end
-        H_reduced = transpose(_reduce_rows(transpose(H_reduced), column_indices, compressed, permute_columns))
+        H_reduced = transpose(_reduce_rows(transpose(H_reduced), column_indices, compressed, permute_columns, column_target))
     end
     return H_reduced
 end
@@ -99,14 +100,14 @@ end
 weight_reduction(C::Union{AbstractLinearCode, AbstractLDPCCode}; rows::Bool = true,
     row_indices::AbstractVector{Int} = Int[], permute_rows::Bool = true, row_target::Int = 3,
     columns::Bool = true, column_indices::AbstractVector{Int} = Int[], permute_columns::Bool = true,
-    column_target::Int = 3, compressed::Bool = false, seed = nothing)
+    column_target::Int = 3, compressed::Bool = false, seed::Union{Nothing, Int} = nothing)
 
-Return the weight-reduced code of `C` with the given arguments.
+Return the code with the weight-reduced parity-check matrix of `C` with the given arguments.
 """
 function weight_reduction(C::Union{AbstractLinearCode, AbstractLDPCCode}; rows::Bool = true,
     row_indices::AbstractVector{Int} = Int[], permute_rows::Bool = true, row_target::Int = 3,
     columns::Bool = true, column_indices::AbstractVector{Int} = Int[], permute_columns::Bool = true,
-    column_target::Int = 3, compressed::Bool = false, seed = nothing)
+    column_target::Int = 3, compressed::Bool = false, seed::Union{Nothing, Int} = nothing)
 
     H_reduced = weight_reduction(parity_check_matrix(C), rows = rows, row_indices = row_indices,
         permute_rows = permute_rows, row_target = row_target, columns = columns,
