7bad8f8503542303d0d77179d014e1bee32da937
david.marquis <david.a.marquis@gmail.com>
2025-02-20 16:22:19 -0500

    Clean

--- benchmarks/weight_dist_bench.jl
+++ benchmarks/weight_dist_bench.jl
@@ -4,12 +4,10 @@ using LinearAlgebra
 using BenchmarkTools
 using Random
 using Dates
-# using AllocCheck
-# using Profile
 include("benchmark_utils.jl")
 CodingTheory.Random.seed!(0)
 
-function white_n70_k35(use_mine, terse_description)
+function white_n70_k35(use_new, terse_description)
     F = Oscar.Nemo.Native.GF(2)
     mat = matrix(F, [1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 1 0 0 0 0 1 1 0 1 1 1 0 1 0 1 1 1 1 0 1 1 0 1 1 0 0 1 0 0 1 1 0 1;
     0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 1 1 1 1 1 0 1 1 0 1 0 0 1 1 0 1 0 1 1 0 1 1 0 1 1 1 0 0 0 1 0;
@@ -49,7 +47,7 @@ function white_n70_k35(use_mine, terse_description)
 
     num_thrds = Threads.nthreads()
     println("Benchmarking with $num_thrds threads.")
-    if use_mine
+    if use_new
         function_description = "_minimum_distance_BZ(codeN70K35,:Brouwer;false)"
         terse_description ? println(function_description) : println(benchmark_description(function_description))
         return @benchmark CodingTheory._minimum_distance_BZ(codeN70K35, info_set_alg = :Brouwer; verbose = false) setup = (codeN70K35=LinearCode($mat, false, false))
@@ -62,5 +60,4 @@ end
 
 use_new=false
 t = white_n70_k35(use_new, true)
-# t = test_white_105(use_mine)
 t
